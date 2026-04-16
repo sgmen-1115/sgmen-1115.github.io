4956173953c59aba08d450a3a6f8c179ce39e57f
sgmen-1115 <sakurao.hideyuki@gmail.com>
2026-04-16 17:36:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,24 @@
-const CACHE_NAME = 'ver1';
+const CACHE_NAME = "app-cache-v2026-04-16-1";
 const urlsToCache = ['./index.html'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", (event) => {
+self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", (event) => {
+event.waitUntil(
+caches.keys().then((keys) =>
+Promise.all(
+keys.map((key) => (key !== CACHE_NAME ? caches.delete(key) : Promise.resolve()))
+)
+).then(() => self.clients.claim())
+);
+});
+
+self.addEventListener("fetch", (event) => {
+if (event.request.mode === "navigate") {
+event.respondWith(
+fetch(event.request).catch(() => caches.match(event.request))
+);
+}
 });
